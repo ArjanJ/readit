3d89f7fb930745cf12fb705d891cfb98a2907a22
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-02-26 13:50:04 -0800

    move api http utils here

---
+++ src/api/Utils.js
@@ -0,0 +1,16 @@
+const Utils = {
+	checkStatus: (response) => {
+		if (response.status >= 200 && response.status < 300) {
+		  return response;
+		} else {
+		  const error = new Error(response.statusText);
+		  error.response = response;
+		  throw error;
+		}
+	},
+	parseJSON: (response) => {
+		return response.json();
+	}
+}
+
+export default Utils;

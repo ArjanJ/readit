c326e5d9bf7d23fccc371955c13c57eebe454b49
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-04 14:51:55 -0800

    ..

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = isDeveloping ? 8080 : process.env.PORT;
 const app = express();
 
 if (!(isDeveloping)) {
+	console.log('is production');
 	app.use(express.static(__dirname + './dist'));
 	app.get('*', function response(req, res) {
 	  res.sendFile(path.join(__dirname, './dist/index.html'));
@@ -17,6 +18,7 @@ if (!(isDeveloping)) {
 }
 
 if (isDeveloping) {
+	console.log('is developing');
 	const webpack = require('webpack');
 	const webpackMiddleware = require('webpack-dev-middleware');
 	const webpackHotMiddleware = require('webpack-hot-middleware');

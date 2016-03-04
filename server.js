a26f3e8bc94ad1eee6361da1826520ae9b0664d0
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-04 14:42:16 -0800

    ...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
+
 const express = require('express');
 const path = require('path');
 
@@ -8,8 +9,14 @@ const port = isDeveloping ? 8080 : process.env.PORT;
 
 const app = express();
 
-if (isDeveloping) {
+if (!(isDeveloping)) {
+	app.use(express.static(__dirname + './dist'));
+	app.get('*', function response(req, res) {
+	  res.sendFile(path.join(__dirname, './dist/index.html'));
+	});
+}
 
+if (isDeveloping) {
 	const webpack = require('webpack');
 	const webpackMiddleware = require('webpack-dev-middleware');
 	const webpackHotMiddleware = require('webpack-hot-middleware');
@@ -34,11 +41,6 @@ if (isDeveloping) {
   app.get('*', function response(req, res) {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
     res.end();
-  });
-} else {
-  app.use(express.static(__dirname + './dist'));
-  app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, './dist/index.html'));
   });
 }
 
